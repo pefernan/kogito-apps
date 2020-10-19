e61c831f7b02afe20d86b909c5676e32db9ec97e
Pere Fernàndez <pere.fernandez@gmail.com>
2020-10-19 17:39:19 +0200

    KOGITO-3438: generation POC deps

---
+++ kogito-html-form-generator/src/main/java/org/kogito/html/Main.java
@@ -0,0 +1,13 @@
+package org.kogito.html;
+
+import com.github.victools.jsonschema.generator.SchemaGenerator;
+import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gson
+    }
+
+}
