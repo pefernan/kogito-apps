82428d9312a9c76e6eb48d211ab3f98287284db7
Pere Fernàndez <pere.fernandez@gmail.com>
2020-11-19 11:03:02 +0100

    - tests

--- kogito-html-form-generator/src/main/java/org/kogito/html/template/Main.java
+++ kogito-html-form-generator/src/main/java/org/kogito/html/template/Main.java
@@ -21,35 +21,35 @@ import org.kogito.html.template.model.impl.TextField;
 public class Main {
 
     public static void main(String[] args) {
-        FormSettingsImpl form = new FormSettingsImpl();
-
-        List<Option> options = new ArrayList<>();
-        options.add(new Option("married", "Married"));
-        options.add(new Option("single", "Single"));
-        options.add(new Option("divorced", "Divorced"));
-
-        FieldSettingsImpl impl = new FieldSettingsImpl("state2", InputType.SELECT, "State 2");
-        impl.set("options", options);
-        impl.setReadOnly(true);
-
-        form.addField(new TextField("name", "Name"))
-                .addField(new IntegerField("age", "Age").setReadOnly(true))
-                .addField(new NumberField("qualifications", "Qualification"))
-                .addField(new DateField("birthday", "Birthday", "datetime-local"))
-                .addField(new Checkbox("accept", "Accept email notifications").setReadOnly(true))
-                .addField(new NestedField("person", "Person")
-                                  .addField(new TextField("name", "Name"))
-                                  .addField(new IntegerField("age", "Age"))
-                                  .addField(new NumberField("qualifications", "Qualification"))
-                                  .addField(new DateField("birthday", "Birthday", "datetime-local"))
-                .addField(new Select("state", "State").addOption("married", "Married").addOption("single", "Single")))
-                .addField(impl);
-
-        Map<String, Object> templateContext = new HashMap<>();
-        templateContext.put("form", form);
-
-        StringBuffer code = TemplateWriter.writeTemplate(templateContext);
-
-        System.out.println(Jsoup.parse(code.toString()));
+//        FormSettingsImpl form = new FormSettingsImpl();
+//
+//        List<Option> options = new ArrayList<>();
+//        options.add(new Option("married", "Married"));
+//        options.add(new Option("single", "Single"));
+//        options.add(new Option("divorced", "Divorced"));
+//
+//        FieldSettingsImpl impl = new FieldSettingsImpl("state2", InputType.SELECT, "State 2");
+//        impl.set("options", options);
+//        impl.setReadOnly(true);
+//
+//        form.addField(new TextField("name", "Name"))
+//                .addField(new IntegerField("age", "Age").setReadOnly(true))
+//                .addField(new NumberField("qualifications", "Qualification"))
+//                .addField(new DateField("birthday", "Birthday", "datetime-local"))
+//                .addField(new Checkbox("accept", "Accept email notifications").setReadOnly(true))
+//                .addField(new NestedField("person", "Person")
+//                                  .addField(new TextField("name", "Name"))
+//                                  .addField(new IntegerField("age", "Age"))
+//                                  .addField(new NumberField("qualifications", "Qualification"))
+//                                  .addField(new DateField("birthday", "Birthday", "datetime-local"))
+//                .addField(new Select("state", "State").addOption("married", "Married").addOption("single", "Single")))
+//                .addField(impl);
+//
+//        Map<String, Object> templateContext = new HashMap<>();
+//        templateContext.put("form", form);
+//
+//        StringBuffer code = TemplateWriter.writeTemplate(templateContext);
+//
+//        System.out.println(Jsoup.parse(code.toString()));
     }
 }
